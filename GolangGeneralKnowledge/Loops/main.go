1d57f868cb8032dae1e5bd140a0089d8c60fa6ad
Kadir GÖNEN <kadirgnen3@gmail.com>
2022-05-12 17:22:00 +0300

    Add to pointers for Golang

---
+++ GolangGeneralKnowledge/Loops/main.go
@@ -0,0 +1,23 @@
+package loops
+
+import "fmt"
+
+func main() {
+
+	var myString string = "Hello world!"
+
+	for i := 0; i < 5; i++ {
+		fmt.Println(myString)
+	}
+	j := 1
+	for j <= 2 {
+		fmt.Println("Good bye Real world!!")
+		j++
+	}
+
+	//for but like while
+	sum := 1
+	for sum <= 10 {
+		sum += sum
+		fmt.Println(sum)
+	}
