7ae71853470bc0eaaddc9272d5969faea8f36d5d
Kadir GÖNEN <kadirgnen3@gmail.com>
2022-05-13 22:03:10 +0300

    Add to pointers
    Add to pointers

---
+++ GolangGeneralKnowledge/Pointers/main.go
@@ -0,0 +1,23 @@
+package Pointers
+
+import (
+	"fmt"
+)
+
+func Process() {
+	var myString string = "Green"
+
+	//log.Println("myString is set to", myString)
+	fmt.Println("myString is set to", myString)
+	changeUsingPointer(&myString)
+	fmt.Println("after func call myString is set to", myString)
+	fmt.Println("myString is point", &myString)
+}
+
+func changeUsingPointer(s *string) {
+	fmt.Println("s is set to", s)
+	newValue := "Red"
+	*s = newValue
+}
+
+//map ve slice için bir kopya oluşmaz. Pointer kullanmasak da pointer kullanmışız gibi davranır
