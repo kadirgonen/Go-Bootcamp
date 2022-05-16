a932570c7b4f79b5ddf72bc4324f3cf4b4037a33
Kadir GÖNEN <kadirgnen3@gmail.com>
2022-05-16 19:14:23 +0300

    Add to restfulapi for Golang
    Add to restfulapi for Golang

---
+++ GolangGeneralKnowledge/restfulapi/main.go
@@ -0,0 +1,52 @@
+package restful
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+//https://mholt.github.io/json-to-go/ U can use this generator.
+type Todo struct {
+	UserId    int    `json:"userId"`
+	Id        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+//GET
+func Main() {
+	response, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer response.Body.Close() //U should close channel always
+	bodyBytes, _ := ioutil.ReadAll(response.Body)
+	bodyString := string(bodyBytes)
+	fmt.Println(bodyString)
+
+	var todo Todo
+	json.Unmarshal(bodyBytes, &todo) //binding
+	fmt.Println(todo)
+}
+
+//POST
+func Main2() {
+	todo := Todo{1, 2, "Dummy Task", false}
+	jsonTodo, _ := json.Marshal(todo)
+	response, err := http.Post("https://jsonplaceholder.typicode.com/todos", "application/json;charset=utf-8", bytes.NewBuffer(jsonTodo))
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer response.Body.Close()
+
+	bodyBytes, _ := ioutil.ReadAll(response.Body)
+	bodyString := string(bodyBytes)
+	fmt.Println(bodyString)
+
+	var todoResponse Todo
+	json.Unmarshal(bodyBytes, &todoResponse) //binding
+	fmt.Println(todoResponse)
+}
